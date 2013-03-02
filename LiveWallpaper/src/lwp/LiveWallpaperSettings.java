40ad8ec5969240dbb0158cd497ec6f4eda4566fa
Chibi Kaguya <wststreet2@yahoo.com>
2013-03-02 23:07:14 +0200

    added None choice

--- LiveWallpaper/src/lwp/LiveWallpaperSettings.java
+++ LiveWallpaper/src/lwp/LiveWallpaperSettings.java
@@ -1,6 +1,5 @@
 package lwp;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,41 +11,43 @@ import android.preference.PreferenceActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class LiveWallpaperSettings extends PreferenceActivity
-    implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class LiveWallpaperSettings extends PreferenceActivity implements
+		SharedPreferences.OnSharedPreferenceChangeListener {
 	List<CharSequence> myList = new ArrayList<CharSequence>();
 	List<CharSequence> myList2 = new ArrayList<CharSequence>();
 	Spinner spinner, spinner_fr;
-	ArrayAdapter <CharSequence>adapter, adapter2;
-	
-	
-    protected void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setContentView(R.layout.settings);
-       spinner = (Spinner)this.findViewById(R.id.spinner1);
-       spinner_fr = (Spinner)this.findViewById(R.id.spinner2);
-        myList.add("Random");
-        myList.add("Gradient");
-        
-        myList2.add("Pizza");
-        
-        adapter  = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item,myList);
-        adapter2 = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item,myList2);
-        spinner.setAdapter(adapter);
-        spinner_fr.setAdapter(adapter2);
-    }
+	ArrayAdapter<CharSequence> adapter, adapter2;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+	protected void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.settings);
+		spinner = (Spinner) this.findViewById(R.id.spinner1);
+		spinner_fr = (Spinner) this.findViewById(R.id.spinner2);
+		myList.add("None");
+		myList.add("Random");
+		myList.add("Gradient");
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+		myList2.add("Pizza");
 
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-            String key) {
-    }
+		adapter = new ArrayAdapter<CharSequence>(this,
+				android.R.layout.simple_spinner_item, myList);
+		adapter2 = new ArrayAdapter<CharSequence>(this,
+				android.R.layout.simple_spinner_item, myList2);
+		spinner.setAdapter(adapter);
+		spinner_fr.setAdapter(adapter2);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+	}
 }
