c81642767efc352ae54c0766dd859c5016a2ca30
xxx <xxx@xxx-PC>
2013-03-02 20:54:16 +0200

    Am scos pasarea de parametrii

--- LiveWallpaper/src/lwp/EyeCandyGradient.java
+++ LiveWallpaper/src/lwp/EyeCandyGradient.java
@@ -2,10 +2,11 @@ package lwp;
 
 import java.util.Random;
 
+
 public class EyeCandyGradient extends EyeCandy {
 
 	private int[][] gradient;
-	private int[][] gr;
+    private int[][] gradCopy;
 	private int width, height;
 
 	public EyeCandyGradient() {
@@ -14,46 +15,49 @@ public class EyeCandyGradient extends EyeCandy {
 
 	@Override
 	public final int[][] draw(int[][] matrix) {
-		for (int i = 0; i < width; i++)
-			for (int j = 0; j < height; j++)
-				gr[i][j] = gradient[i][j];
-		return gr;
+		gradCopy = new int[width][height];
+		
+		for (int i = 0; i < height; i++) 
+			for (int j = 0; j < width; j++)
+				gradCopy[j][i] = gradient[j][i];
+		
+		return gradCopy;
 	}
+	
 
 	@Override
 	public void init() {
 		width = MyWallpaper.getLCD_WIDTH();
 		height = MyWallpaper.getLCD_HEIGHT();
 		gradient = new int[width][height];
-		gr = new int[width][height];
-		gradient = drawGradient(gradient);
-		gradient = ditherGradient(gradient);
+		
+		drawGradient();
+		ditherGradient();
 	}
 
-	private int[][] drawGradient(int[][] g) {
+	private void drawGradient() {
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++)
-				g[j][i] = (int) (i * (255.0 / height));
+				gradient[j][i] = (int) (i * (255.0 / height));
 		}
 
-		return g;
 	}
 
-	private int[][] ditherGradient(int[][] g) {
+	private void ditherGradient() {
 		Random r = new Random();
 
 		for (int i = 1; i < width - 1; i += 3) {
 			for (int j = 1; j < height - 1; j += 3) {
-				int original = g[i][j];
+				int original = gradient[i][j];
 				for (int k = i - 1; k <= i + 1; k++)
 					for (int l = j - 1; l <= j + 1; l++) {
 						if (r.nextInt(100) <= 100 * (original / 255.0))
-							g[k][l] = 0;
+							gradient[k][l] = 0;
 						else
-							g[k][l] = 1;
+							gradient[k][l] = 1;
 					}
 			}
 		}
-		return g;
+		
 	}
 }
