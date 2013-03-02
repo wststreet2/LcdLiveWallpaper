7d7e66ad8c72b1b4fce873a7ff2133fd5dd15f81
Chibi Kaguya <wststreet2@yahoo.com>
2013-03-03 00:13:18 +0200

    remember spinner location (part 1)

--- LiveWallpaper/src/lwp/LiveWallpaperSettings.java
+++ LiveWallpaper/src/lwp/LiveWallpaperSettings.java
@@ -8,19 +8,24 @@ import org.kamehamehaaa.android.livewallpaper.R;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class LiveWallpaperSettings extends PreferenceActivity implements
 		SharedPreferences.OnSharedPreferenceChangeListener {
-	List<CharSequence> myList = new ArrayList<CharSequence>();
-	List<CharSequence> myList2 = new ArrayList<CharSequence>();
-	Spinner spinner, spinner_fr;
-	ArrayAdapter<CharSequence> adapter, adapter2;
+	private List<CharSequence> myList = new ArrayList<CharSequence>();
+	private List<CharSequence> myList2 = new ArrayList<CharSequence>();
+	private Spinner spinner, spinner_fr;
+	private ArrayAdapter<CharSequence> adapter, adapter2;
+	private static final String PREFS_NAME = "LcdLiveWallpaperSettings";
+	
 
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.settings);
+		
 		spinner = (Spinner) this.findViewById(R.id.spinner1);
 		spinner_fr = (Spinner) this.findViewById(R.id.spinner2);
 		myList.add("None");
@@ -35,6 +40,18 @@ public class LiveWallpaperSettings extends PreferenceActivity implements
 				android.R.layout.simple_spinner_item, myList2);
 		spinner.setAdapter(adapter);
 		spinner_fr.setAdapter(adapter2);
+		
+		spinner.setOnItemSelectedListener(new EyeCandyListener());
+		spinner.setSelection(0);
+		
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		for(int i = 0; i< spinner.getCount(); i++)
+		{
+			if(spinner.getItemAtPosition(i).equals(settings.getString("eyeCandy", "None")))
+				spinner.setSelection(i);
+		}
+		
+		
 	}
 
 	@Override
@@ -50,4 +67,18 @@ public class LiveWallpaperSettings extends PreferenceActivity implements
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
 	}
+}
+
+class EyeCandyListener implements AdapterView.OnItemSelectedListener
+{
+
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		
+	}
+
+	public void onNothingSelected(AdapterView<?> arg0) {
+		
+	}
+	
 }
