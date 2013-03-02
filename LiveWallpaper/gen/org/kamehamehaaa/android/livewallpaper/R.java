50b421123d7eb88b296b1e03b5c4c69d49fef832
Chibi Kaguya <wststreet2@yahoo.com>
2013-03-03 00:02:18 +0200

    adjusted settings layout

--- LiveWallpaper/gen/org/kamehamehaaa/android/livewallpaper/R.java
+++ LiveWallpaper/gen/org/kamehamehaaa/android/livewallpaper/R.java
@@ -16,9 +16,11 @@ public final class R {
     }
     public static final class id {
         public static final int adView=0x7f070001;
+        public static final int eyeCandyTextView=0x7f070002;
         public static final int mainLayout=0x7f070000;
-        public static final int spinner1=0x7f070002;
-        public static final int spinner2=0x7f070003;
+        public static final int spinner1=0x7f070003;
+        public static final int spinner2=0x7f070005;
+        public static final int textView2=0x7f070004;
     }
     public static final class layout {
         public static final int settings=0x7f030000;
