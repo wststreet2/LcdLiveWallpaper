0c7cb07674fa407ed44ebd1977c692e5e53423c2
wststreet <wststreet2@yahoo.com>
2013-03-03 18:00:30 +0200

    created branch

--- LiveWallpaper/src/lwp/LiveWallpaperSettings.java
+++ LiveWallpaper/src/lwp/LiveWallpaperSettings.java
@@ -27,50 +27,7 @@ public class LiveWallpaperSettings extends PreferenceActivity {
 
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		setContentView(R.layout.settings);
-
-		spinner = (Spinner) this.findViewById(R.id.spinner1);
-		spinner_fr = (Spinner) this.findViewById(R.id.spinner2);
-		myList.add("None");
-		myList.add("Random");
-		myList.add("Gradient");
-
-		myList2.add("Pizza");
-
-		adapter = new ArrayAdapter<CharSequence>(this,
-				android.R.layout.simple_spinner_item, myList);
-		adapter2 = new ArrayAdapter<CharSequence>(this,
-				android.R.layout.simple_spinner_item, myList2);
-		spinner.setAdapter(adapter);
-		spinner_fr.setAdapter(adapter2);
-
-		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				String eyeCandyName = (String) spinner.getItemAtPosition(arg2);
-				SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-				SharedPreferences.Editor editor = settings.edit();
-
-				editor.putString("eyeCandy", eyeCandyName);
-				editor.commit();
-
-				MyWallpaper.setEyeCandy(eyeCandyName);
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-		});
-		spinner.setSelection(0);
-
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		for (int i = 0; i < spinner.getCount(); i++) {
-			if (spinner.getItemAtPosition(i).equals(
-					settings.getString("eyeCandy", "None")))
-				spinner.setSelection(i);
-		}
+		
 
 	}
 
