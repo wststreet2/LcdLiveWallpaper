a9f352d2a7d3905b2521d175cf37bbb8141aaa4b
wststreet <wststreet2@yahoo.com>
2013-03-09 19:39:49 +0200

    changed pixel size conditions

--- LiveWallpaper/src/lwp/LCDLiveWallpaper.java
+++ LiveWallpaper/src/lwp/LCDLiveWallpaper.java
@@ -151,11 +151,11 @@ public class LCDLiveWallpaper extends WallpaperService {
 			} else {
 				d.getMetrics(m);
 			}
-			if (m.heightPixels >= 500) {
+			if (m.heightPixels >= 800) {
 				LCD_WIDTH = m.widthPixels / 10;
 				LCD_HEIGHT = m.heightPixels / 10;
 				
-			} else if (m.heightPixels > 400) {
+			} else if (m.heightPixels > 480) {
 				LCD_WIDTH = m.widthPixels / 5;
 				LCD_HEIGHT = m.heightPixels / 5;
 				
