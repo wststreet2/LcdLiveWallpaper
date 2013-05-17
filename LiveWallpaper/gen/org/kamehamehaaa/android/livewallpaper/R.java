97e424c6d5bb9d91815fc6dc9bd8e64674fcaa9a
Chibi Kaguya <wststreet2@yahoo.com>
2013-05-17 19:36:58 +0300

    added support for future implementation of multiple languages

--- LiveWallpaper/gen/org/kamehamehaaa/android/livewallpaper/R.java
+++ LiveWallpaper/gen/org/kamehamehaaa/android/livewallpaper/R.java
@@ -9,11 +9,34 @@ package org.kamehamehaaa.android.livewallpaper;
 
 public final class R {
     public static final class array {
-        public static final int clockType=0x7f060001;
-        public static final int color_styles=0x7f060003;
-        public static final int color_styles_values=0x7f060004;
-        public static final int dateFormat=0x7f060002;
-        public static final int eyeCandyArray=0x7f060000;
+        public static final int clockType=0x7f060002;
+        public static final int clockType_values=0x7f060005;
+        /**  SETTINGS MENU END 
+ SETTINGS MENU ARRAYS 
+ SETTINGS MENU END 
+ SETTINGS MENU ARRAYS 
+         */
+        public static final int color_styles=0x7f060000;
+        public static final int color_styles_values=0x7f060007;
+        public static final int dateFormat=0x7f060003;
+        public static final int dateFormat_values=0x7f060006;
+        public static final int eyeCandyArray=0x7f060001;
+        /**  SETTINGS MENU ARRAYS END 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ SETTINGS MENU ARRAY VALUES 
+ SETTINGS MENU ARRAYS END 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ DO NOT TRANSLATE BELOW THIS LINE!!!! 
+ SETTINGS MENU ARRAY VALUES 
+         */
+        public static final int eyeCandyArray_values=0x7f060004;
     }
     public static final class attr {
         /** <p>Must be an integer value, such as "<code>100</code>".
@@ -49,9 +72,52 @@ containing a value of this type.
         public static final int dialog_slider=0x7f030001;
     }
     public static final class string {
+        public static final int about_creators_desc=0x7f050025;
+        public static final int about_creators_title=0x7f050024;
+        public static final int about_donate_desc=0x7f050028;
+        public static final int about_donate_title=0x7f050027;
+        public static final int about_email_desc=0x7f05002a;
+        public static final int about_email_title=0x7f050029;
+        public static final int about_rate=0x7f050026;
+        public static final int about_settings=0x7f050023;
         public static final int app_name=0x7f050000;
+        public static final int colors_bg=0x7f050011;
+        public static final int colors_custom=0x7f050010;
+        public static final int colors_px=0x7f050012;
+        public static final int colors_set=0x7f05000f;
+        public static final int colors_settings=0x7f05000e;
+        public static final int datetime_bigbin_desc=0x7f05001f;
+        public static final int datetime_bigbin_title=0x7f05001e;
+        public static final int datetime_clocktype_desc=0x7f05001a;
+        public static final int datetime_clocktype_title=0x7f050019;
+        public static final int datetime_dateformat=0x7f05001b;
+        public static final int datetime_invertbin_desc=0x7f05001d;
+        public static final int datetime_invertbin_title=0x7f05001c;
+        public static final int datetime_settings=0x7f050013;
+        public static final int datetime_showclock_desc=0x7f050015;
+        public static final int datetime_showclock_title=0x7f050014;
+        public static final int datetime_showdate_desc=0x7f050017;
+        public static final int datetime_showdate_title=0x7f050016;
+        public static final int datetime_specific_settings=0x7f050018;
+        public static final int eye_candy_desc=0x7f050005;
+        public static final int eye_candy_specific_settings=0x7f050006;
+        public static final int eye_candy_title=0x7f050004;
+        /**  SETTINGS MENU TEXT 
+ SETTINGS MENU TEXT 
+         */
+        public static final int general_settings=0x7f050003;
+        public static final int perf_framerate_desc=0x7f050022;
+        public static final int perf_framerate_title=0x7f050021;
+        public static final int perf_settings=0x7f050020;
+        public static final int random_density_desc=0x7f050009;
+        public static final int random_density_title=0x7f050008;
+        public static final int random_settings=0x7f050007;
         public static final int settings_title=0x7f050001;
         public static final int wallpaper_desc=0x7f050002;
+        public static final int waterfall_maxnum_desc=0x7f05000c;
+        public static final int waterfall_maxnum_title=0x7f05000b;
+        public static final int waterfall_overlap=0x7f05000d;
+        public static final int waterfall_settings=0x7f05000a;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
