0f0645e1c481fdd74156f3370452deb504a64e02
wststreet <wststreet2@yahoo.com>
2013-06-07 23:27:15 +0300

    wat?

--- LiveWallpaper/src/org/kamehamehaaa/android/livewallpaper/settings/LCDLiveWallpaperSettings.java
+++ LiveWallpaper/src/org/kamehamehaaa/android/livewallpaper/settings/LCDLiveWallpaperSettings.java
@@ -6,6 +6,7 @@ import org.kamehamehaaa.android.livewallpaper.candies.EyeCandyWaterfall;
 import org.kamehamehaaa.android.livewallpaper.engine.LCDLiveWallpaper;
 import org.kamehamehaaa.android.livewallpaper.engine.WriteClass;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
