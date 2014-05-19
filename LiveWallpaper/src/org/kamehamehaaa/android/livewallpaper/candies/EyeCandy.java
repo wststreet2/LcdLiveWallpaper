db1f2e75fce6dca48ad3d3a2fdd221e0ba385d11
Chibi Sun <wststreet2@yahoo.com>
2014-05-19 18:51:54 +0300

    deleted old packages

--- LiveWallpaper/src/org/kamehamehaaa/android/livewallpaper/candies/EyeCandy.java
+++
@@ -1,7 +0,0 @@
-package org.kamehamehaaa.android.livewallpaper.candies;
-
-public abstract class EyeCandy {
-
-	public abstract boolean[][] draw(boolean[][] matrix);
-	public abstract void init();
-}
